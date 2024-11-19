05735a2dd5a33477e71c67265872f4543f780bc7
Myakot <cstepan@yandex.ru>
2024-11-19 14:48:27 +0700

    updated project's structure, sqlite3 db is now created in it's own folder

--- test_bookstore/main/settings.py
+++ test_bookstore/main/settings.py
@@ -88,9 +88,9 @@ WSGI_APPLICATION = "main.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'data', 'db.sqlite3'),
     }
 }
 
